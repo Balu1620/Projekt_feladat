eea300762b0c96361426186c9d56f4a22906e637
Balu1620 <balikomunka@gmail.com>
2025-02-03 14:38:15 +0100

    motorokkereseskesz

--- resources/views/motors/index.blade.php
+++ resources/views/motors/index.blade.php
@@ -2,82 +2,92 @@
 
 @section('content')
 
-<div class="container">
-    <h2 class="text-center">Motorok</h2>
+<div class="mcontent">
+    <h2 style="text-align: center;">Motorok</h2>
 
-    <!-- Szűrő -->
-    <button class="btn btn-primary mb-4 ms-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#filterCanvas"
-        aria-controls="filterCanvas"> Szűrés <i class="bi bi-sliders"></i>
+    <button class="open-btn btn btn-primary mb-4" type="button" data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+        Szűrés <i class="fa-solid fa-sliders"></i>
     </button>
 
-    <div id="filter">
-        <div class="offcanvas offcanvas-start filter" tabindex="-1" id="filterCanvas"
-            aria-labelledby="filterCanvasLabel">
-            <div class="offcanvas-header">
-                <h4 class="offcanvas-title" id="filterCanvasLabel">Szűrés</h4>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <form method="GET" action="{{ route('motors.index') }}">
-                    <div class="mb-3">
-                        <select name="Marka" class="form-select">
+    <div class="filter offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions"
+        aria-labelledby="offcanvasWithBothOptionsLabel">
+        <div class="offcanvas-header">
+            <h4 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Szűrés</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <form method="GET">
+                <div class="row container-fluid">
+                    <div class="col-12 mb-2">
+                        <select class="form-control" name="brand">
                             <option value="" disabled selected>Márka</option>
-                            @foreach($motors as $item)
-                                <option value="{{ $item->brand }}">{{ $item->brand }}</option>
+                            @foreach($brands as $brand)
+                                <option value="{{ $brand->brand }}">{{ $brand->brand }}</option>
                             @endforeach
                         </select>
                     </div>
-                    <div class="mb-3">
-                        <select name="Kor" class="form-select">
+                    <div class="col-12 mb-2">
+                        <select class="form-control" name="location">
+                            <option value="" disabled selected>Kerület</option>
+                            @foreach($locations as $location)
+                                <option value="{{ $location->location }}">{{ $location->location }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col-12 mb-2">
+                        <select class="form-control" name="year">
                             <option value="" disabled selected>Kor</option>
-                            @foreach($motors as $item)
-                                <option value="{{ $item->year }}">{{ $item->year }}</option>
+                            @foreach($years as $year)
+                                <option value="{{ $year->year }}">{{ $year->year }}</option>
                             @endforeach
                         </select>
                     </div>
-                    <div class="mb-3">
-                        <select name="Sebessegvalto" class="form-select">
-                            <option value="" disabled selected>Sebességváltó</option>
-                            @foreach($motors as $item)
-                                <option value="{{ $item->gearbox }}">{{ $item->gearbox }}</option>
+                    <div class="col-12 mb-2">
+                        <select class="form-control" name="gearbox">
+                            <option value="" disabled selected>Sebesség váltó</option>
+                            @foreach($gearboxes as $gearbox)
+                                <option value="{{ $gearbox->gearbox }}">{{ $gearbox->gearbox }}</option>
                             @endforeach
                         </select>
                     </div>
-                    <div class="mb-3">
-                        <select name="Uzemanyag" class="form-select">
-                            <option value="" disabled selected>Üzemanyag</option>
-                            <option value="Elektromos">Elektromos</option>
-                            <!-- Egyéb üzemanyagok -->
-                        </select>
+                    <div id="radio" class="mt-1">
+                        <div class="form-switch form-check">
+                            <label for="petrol">Benzin:</label>
+                            <input id="petrol" class="form-check-input" type="checkbox" name="fuel" value="B">
+                        </div>
+                        <div class="form-check form-switch">
+                            <label for="electro">Elektromos:</label>
+                            <input id="electro" class="form-check-input" type="checkbox" name="fuel" value="E">
+                        </div>
                     </div>
-
-                    <div class="text-end mt-4">
-                        <button type="submit" name="submit" class="btn btn-primary ">Szűrés</button>
+                    <div class="col-12 mt-2">
+                        <button class="btn btn-secondary w-100">Szűrés</button>
                     </div>
-
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
     </div>
-    <!-- Motorok listája -->
-    <div class="row">
-        @foreach($motors as $item)
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $item->brand }}</h5>
-                        <p class="card-text">Kor: {{ $item->year }}</p>
-                        <p class="card-text">Sebességváltó: {{ $item->gearbox }}</p>
-                        <p class="card-text">Üzemanyag: 
 
-                        @if ($item->fuel === "B")
-                            Benzines
-                        @elseif($item->fuel === "E")
-                            Elektromos
-                        @else
-                        @endif
-                        </p>
-                        <p class="card-text">Helyszín: {{$item->location}}</p>
+    <div class="row" id="margin">
+        @foreach($motorcycles as $motorcycle)
+            <div class="col-md-4 mb-4 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="Motor képe">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <span class="brand">{{ $motorcycle->brand }}</span> - {{ number_format($motorcycle->price) }}
+                            Ft/nap
+                        </h5>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">{{ $motorcycle->brand }} {{ $motorcycle->type }}</li>
+                            <li class="list-group-item">{{ $motorcycle->power }}</li>
+                            <li class="list-group-item">{{ $motorcycle->gearbox }}</li>
+                            <li class="list-group-item">{{ $motorcycle->location }}</li>
+                        </ul>
+                        <div class="card-body">
+                            <a href="#" class="btn btn-secondary">Részletek</a>
+                        </div>
                     </div>
                 </div>
             </div>
