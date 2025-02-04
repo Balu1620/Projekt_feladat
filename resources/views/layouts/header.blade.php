4a2a26d374d87c7057a519675c4f0145101a42d0
Kadasi-Emese <kadasimesi@gmail.com>
2025-02-04 18:56:08 +0100

    about 2.0

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -125,9 +125,6 @@
 					<li class="mb-1">
 						<a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="{{ route('location') }}">Helyszínek</a>
 					</li>
-					<li class="mb-1">
-						<a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Bérlés</a>
-					</li>
 				</ul>
 			</div>
 			<div class="mt-auto">
