9c70be0d2d1eb60ff7a3b53a88b9404a563abb2d
Balu1620 <balikomunka@gmail.com>
2025-02-13 15:17:11 +0100

    ja

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,13 @@ use App\Http\Controllers\MotorcycleAPIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
 
 Route::apiResource("/apiMotorcycle", MotorcycleAPIController::class);
+
+Route::post("/proba", function(){
+    return response()->json();
+});
