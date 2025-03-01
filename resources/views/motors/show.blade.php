c49d7db604b3f525dcf030cbd4e96964e4eb5baf
Balu1620 <balikomunka@gmail.com>
2025-03-01 09:49:20 +0100

    javit

--- resources/views/motors/show.blade.php
+++ resources/views/motors/show.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 
-
+<div class="ms-10 mt-5">
+<a href="{{ url()->previous() }}" class="btn btn-primary"><i class="bi bi-arrow-left"></i> View Back All Categories</a>
+</div>
 <div class="motor-details">
     <div class="motor-image">
         <img src="{{ asset('storage/img/placeholder.png') }}" alt="Motor image" />
@@ -14,13 +16,11 @@
             <li><i class="fa fa-plug"></i> {{ $motor->power }} LE</li> <!-- A 'fa-plug' ikon -->
             <li><i class="fa fa-calendar-alt"></i> Évjárat: {{ $motor->year }}</li>
             <li><i class="fa fa-cogs"></i> {{ $motor->gearbox }} sebességes váltó</li>
-            <li><i class="fa fa-users"></i> Ülés {{ $motor->places }} személyes</li>
+            <li><i class="fa fa-users"></i> A Jármű {{ $motor->places }} személyes</li>
             <li><i class="fa fa-map-marker-alt"></i> Location {{ $motor->location }}</li>
         </ul>
         <div class="pricing-buttons">
-            <button class="btn btn-dark">1 nap <span>{{ $motor->price }} Ft</span></button>
-            <button class="btn btn-dark">3-5 nap <span>{{ $motor->price }} Ft</span></button>
-            <button class="btn btn-dark">5+ nap <span>{{ $motor->price }} Ft</span></button>
+            <a href="{{ route("tools.index") }}"><button class="btn btn-dark">1 nap <span>{{ $motor->price }} Ft</span></button></a>
         </div>
     </div>
 </div>
