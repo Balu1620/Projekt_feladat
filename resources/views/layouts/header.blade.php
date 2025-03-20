63fd7cd759d74a8542677cb70b8895b79f43e410
Kadasi-Emese <kadasimesi@gmail.com>
2025-03-20 10:37:38 +0100

    ujra

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -81,6 +81,8 @@
           </div>
         </div>
       </div>
+
+      
       
       <!-- Jobb oldali elem: értesítés és profil -->
       <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
