216342f593eb841953cdb1b7a8447e74ba33718b
Balu1620 <balikomunka@gmail.com>
2025-03-20 10:32:06 +0100

    visszaállitás

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -82,6 +82,8 @@
         </div>
       </div>
       
+      
+
       <!-- Jobb oldali elem: értesítés és profil -->
       <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
         </button>
