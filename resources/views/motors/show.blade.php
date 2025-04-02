42aad0a567ab1cdfe7af3b5ee7218f15a7c00a0f
Kadasi-Emese <kadasimesi@gmail.com>
2025-04-02 14:42:39 +0200

    fix kep

--- resources/views/motors/show.blade.php
+++ resources/views/motors/show.blade.php
@@ -61,7 +61,7 @@
             <div class="row">
                 <!-- Motor kép -->
                 <div class="col-md-8 motor-image-container">
-                    <img src="{{ asset('storage/img/' .$motorcycle->image) }}" alt="Motor image" />
+                    <img src="{{ asset('storage/img/' .$motor->image) }}" alt="Motor image" />
                     <p class="reservation-info">
                         <small>*Foglalás értelmezése: a bérlés időtartama 24 órára vonatkozik. Tehát ha egy adott napon 9
                             órakor átveszed a motort, a bérlés a következő nap 9 óráig érvényes.</small>
